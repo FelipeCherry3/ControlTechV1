45a569b9195fc1643e240e50b10bd028c9699ddb
FelipeCherry3 <felipeelias609@hotmail.com>
2023-11-22 09:07:48 -0300

    Bug resolvido @Id

--- src/main/java/com/losBicos/ControlTechV1/modelos/ProdutoCategoria.java
+++ src/main/java/com/losBicos/ControlTechV1/modelos/ProdutoCategoria.java
@@ -1,18 +1,24 @@
 package com.losBicos.ControlTechV1.modelos;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name = "produto_has_categoria")
+@NoArgsConstructor
 public class ProdutoCategoria {
 
-
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+
     @ManyToOne
     @JoinColumn(name = "id_produto")
     private Ativos produto;
 
-    @Id
+
     @ManyToOne
     @JoinColumn(name = "id_categoria")
     private Categoria categoria;
@@ -22,4 +28,27 @@ public class ProdutoCategoria {
         this.categoria = categoria;
     }
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Ativos getProduto() {
+        return produto;
+    }
+
+    public void setProduto(Ativos produto) {
+        this.produto = produto;
+    }
+
+    public Categoria getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(Categoria categoria) {
+        this.categoria = categoria;
+    }
 }
