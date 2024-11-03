db8caa0e19a0c8b80e45861ac9aa17ad636240e5
FelipeCherry3 <felipeelias609@hotmail.com>
2024-11-03 20:26:08 -0300

    a

--- src/main/java/com/losBicos/ControlTechV1/controladores/UserController.java
+++ src/main/java/com/losBicos/ControlTechV1/controladores/UserController.java
@@ -1,7 +1,13 @@
 package com.losBicos.ControlTechV1.controladores;
 
 import com.losBicos.ControlTechV1.repositories.UsersRepository;
+
+import jakarta.persistence.NonUniqueResultException;
+
 import com.losBicos.ControlTechV1.modelos.Usuario;
+
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
@@ -31,23 +37,25 @@ public class UserController {
     // Rota para fazer login
     @PostMapping(path = "/login")
     public @ResponseBody String fazerLogin(@RequestParam String nome, @RequestParam String user_password) {
-        Usuario usuario = usersRepository.findByNome(nome);
-        if (usuario != null && usuario.getUser_password().equals(user_password)){
+        List<Usuario> usuarios = usersRepository.findByLogin(nome);
+        if (usuarios != null && usuarios.get(0).getUser_password().equals(user_password)){
             //Implementar login XXX Erro de lógica
             return "Login bem-sucedido!";
 
-        } else if (usuario == null) {
-
+        } else if (usuarios == null) {
+            return "Usuário não encontrado!";
+        } else {
+            return "Senha incorreta!";
         }
-    return "Login bem-sucedido!";
     }
+
     @GetMapping(path = "/findbylogin")
     public @ResponseBody Usuario findByLogin(@RequestParam String login) {
-        Usuario usuario = usersRepository.findByLogin(login);
-        if(usuario == null){
-            System.out.println("Sem usuario");
+        List<Usuario> usuarios = usersRepository.findByLogin(login);
+        if (usuarios.size() == 1) {
+            return usuarios.get(0);
+        } else {
+            return usuarios.get(0);
         }
-        System.out.println(usuario.getLogin());
-        return usuario;
     }
 }
--- src/main/java/com/losBicos/ControlTechV1/repositories/UsersRepository.java
+++ src/main/java/com/losBicos/ControlTechV1/repositories/UsersRepository.java
@@ -1,11 +1,14 @@
 package com.losBicos.ControlTechV1.repositories;
 
 import com.losBicos.ControlTechV1.modelos.Usuario;
+
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UsersRepository extends JpaRepository<Usuario, Long> {
     Usuario findByNome(String nome);
-    Usuario findByLogin(String login);
+    List<Usuario> findByLogin(String login);
 }
